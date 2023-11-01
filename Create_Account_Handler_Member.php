7ecde1b1e6a8655ef29e5857136984735f357c9a
david-perez1 <143773766+david-perez1@users.noreply.github.com>
2023-10-31 19:52:56 -0500

    Update Create_Account_Handler_Member.php

--- Create_Account_Handler_Member.php
+++ Create_Account_Handler_Member.php
@@ -2,40 +2,49 @@
 
 include('connection.php');
 
-if (!isset($_POST['password'], $_POST['username'])) {
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
     exit('Please complete the registration form!');
 }
-if (empty($_POST['password']) || empty($_POST['username'])) {
-    exit('Please complete the registration form');
+
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+    exit('Please complete the registration form!');
 }
 
 if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
     exit('Username is not valid!');
 }
+
 if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
     exit('Password must be between 5 and 20 characters long!');
 }
 
-if ($stmt = $connMember->prepare('SELECT member_ID, password FROM users WHERE username = ?')) {
-    $stmt->bind_param('s', $_POST['username']);
+$email = $_POST['email'];
+
+// Validate the email ends with @cougarnet.uh.edu
+if (substr($email, -17) !== '@cougarnet.uh.edu') {
+    exit('Email must end with @cougarnet.uh.edu');
+}
+
+// Check if username or email already exists
+if ($stmt = $connMember->prepare('SELECT member_ID FROM users WHERE username = ? OR email = ?')) {
+    $stmt->bind_param('ss', $_POST['username'], $email);
     $stmt->execute();
     $stmt->store_result();
     if ($stmt->num_rows > 0) {
-        echo 'Username exists, please choose another!';
-    } else {
-        if ($stmt = $connMember->prepare('INSERT INTO users(password, username) VALUES (?, ?)')) {
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            $stmt->bind_param('ss', $password, $_POST['username']);
-            $stmt->execute();
-
-            header('Location: Login_Member.php?accountCreation=success');
-        } else {
-            echo 'Could not prepare statement!';
-        }
-    }
-    $stmt->close();
-} else {
-    echo 'Could not prepare statement!';
+        exit('Username or Email exists, please choose another!');
+    } 
 }
+
+// Insert new user
+if ($stmt = $connMember->prepare('INSERT INTO users (username, password, email) VALUES (?, ?, ?)')) {
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $stmt->bind_param('sss', $_POST['username'], $password, $email);
+    $stmt->execute();
+    header('Location: Login_Member.php?accountCreation=success');
+} else {
+    exit('Could not prepare statement!');
+}
+
+$stmt->close();
 $connMember->close();
 ?>
