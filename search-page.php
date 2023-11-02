80e9bd3254f9115b6bc8bf2db176d0831537ada0
david-perez1 <143773766+david-perez1@users.noreply.github.com>
2023-11-01 22:36:59 -0500

    Update search-page.php

--- search-page.php
+++ search-page.php
@@ -1,7 +1,6 @@
 <?php
-
 include('connection.php');
-include('navloggedin.php')
+include('navloggedin.php');
 ?>
 
 <!DOCTYPE html>
@@ -12,41 +11,36 @@ include('navloggedin.php')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music Search</title>
 </head>
-<body class = "background">
-    <h1 class = "library-title">Music Search</h1>
-        <div class = "main-content">
-            <form method="get" action="">
-                <label for="query">Search:</label>
-                <input type="text" id="query" name="query" required>
-                <button class="create-playlist-button">Search</button>
-            </form>
-        </div>
+<body class="background">
+    <h1 class="library-title">Music Search</h1>
+    <div class="main-content">
+        <form method="get" action="">
+            <label for="query">Search:</label>
+            <input type="text" id="query" name="query" required>
+            <button class="create-playlist-button">Search</button>
+        </form>
+    </div>
     <div id="results">
         <?php
-       
-
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if (isset($_GET['query'])) {
+            $query = mysqli_real_escape_string($con, $_GET['query']);
+            $sql = "SELECT * FROM song WHERE SongTitle LIKE '%$query%' OR Genre LIKE '%$query%' OR artistName LIKE '%$query%'";
             
-            if (isset($_GET['query'])) {
-                $query = $_GET['query'];
-                $stmt = $conn->prepare("SELECT * FROM song WHERE SongTitle LIKE :query OR Genre LIKE :query OR artistName LIKE :query");
-                $stmt->execute(['query' => "%$query%"]);
+            $result = mysqli_query($con, $sql);
 
-                $results = $stmt->fetchAll();
-                
-                if ($results) {
+            if ($result) {
+                $results = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+                if (count($results) > 0) {
                     foreach ($results as $row) {
                         echo "<p>Song ID: " . htmlspecialchars($row['SongID']) . " - Title: " . htmlspecialchars($row['SongTitle']) . " - Artist: " . htmlspecialchars($row['artistName']) . " - Genre: " . htmlspecialchars($row['Genre']) . "</p>";
                     }
                 } else {
                     echo "<p>No results found</p>";
                 }
+            } else {
+                echo "<p>Error executing query: " . htmlspecialchars(mysqli_error($con)) . "</p>";
             }
-
-        } catch(PDOException $e) {
-            echo "<p>Connection failed: " . htmlspecialchars($e->getMessage()) . "</p>";
         }
         ?>
     </div>
