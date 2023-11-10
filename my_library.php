ce6a4e116fe964ad96ab56593416ad5a477d801e
Yonatan <yonatandeneke@gmail.com>
2023-11-09 18:11:21 -0600

    sidebar

--- my_library.php
+++ my_library.php
@@ -1,65 +1,94 @@
 <?php
-include('navloggedin.php');
-include('connection.php');
-include('entities/Playlist.php');
+  include('navloggedin.php');
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Cougify</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Playlist Library</title>
     <style>
-        .different-color {
-            background-color: #9c0202;
-            height: 100%;
-            width: 25%;
-            position: absolute;
-            left: 0;
-            text-align: center;
-        }
+        body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    height: 100vh; /* Set body height to 100% of the viewport height */
+}
 
-        .library-background {
-            background-color: #800000;
-            height: 100%;
-        }
+.container {
+    display: flex;
+    height: 100%; /* Set container height to 100% of the viewport height */
+}
 
-        .centered-list {
-            display: inline-block;
-            text-align: left;
-            color: white;
-        }
+.sidebar {
+    width: 200px;
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+    box-sizing: border-box;
+    height: 100%; /* Set sidebar height to 100% of the viewport height */
+    overflow-y: auto; /* Add a scrollbar if the content overflows */
+}
 
-        .playlist-box {
-            border: 2px solid white;
-            padding: 100%;
-            margin-bottom: 20px;
-            border-radius: 0px;
-           
-        }
+.sidebar h2 {
+    margin-bottom: 20px;
+}
+
+.sidebar ul {
+    list-style: none;
+    padding: 0;
+    margin: 0; /* Remove default margin on ul */
+}
+
+.sidebar li {
+    cursor: pointer;
+    transition: background-color 0.3s ease; /* Add a smooth transition effect */
+    padding: 10px 0; /* Adjust padding for better spacing */
+    line-height: 1.5; /* Adjust line height for better spacing */
+    height: auto; /* Allow the height to adjust based on content */
+    margin-bottom: 0; /* Remove the margin-bottom */
+}
+
+/* Hover effect */
+.sidebar li:hover {
+    background-color: #555;
+}
+
+.content {
+    flex-grow: 1;
+    padding: 20px;
+    box-sizing: border-box;
+}
+
+.content h2 {
+    margin-bottom: 20px;
+}
+
     </style>
 </head>
-
 <body>
-    <div class="library-background">
-        <div class="different-color">
-            <ul class="centered-list">
-                <?php
+    <div class="container">
+        <div class="sidebar">
+            <h2>Your Library</h2>
+            <ul id="playlist">
+            <?php
+                include('connection.php');
                 $uid = $_SESSION['id'];
                 $sql = "SELECT * FROM playlist WHERE playlist.UserID = $uid";
                 $playlists = mysqli_query($con, $sql);
 
                 foreach ($playlists as $playlistData) {
-                    echo "<div class='playlist-box'><li>" . $playlistData['Playlist Title'] . "</li></div>";
+                    echo "<li>" . $playlistData['Playlist Title'] . "</li>";
                 }
-
-                ?>
+            ?>
             </ul>
+        </div>
+        <div class="content">
+            <h2>Playlist Content</h2>
+            <div id="playlist-content">
+                <!-- Playlist content will be displayed here -->
+            </div>
         </div>
     </div>
 </body>
-
 </html>
