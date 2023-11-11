534df233c6dd838f86d824759b9bf4f4e4b92eae
arandrews <116930513+arandrews@users.noreply.github.com>
2023-11-11 01:27:11 -0600

    playlist DELETION

---
+++ deletePlaylist.php
@@ -0,0 +1,36 @@
+<?php
+include('connection.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deletePlaylist'])) {
+    $playlistID = $_POST['playlistID'];
+
+    // Add any necessary validation or security checks here
+
+    $sql = "DELETE FROM playlistsongs WHERE PlaylistID = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $playlistID);
+
+    if ($stmt->execute()) {
+        $sql = "DELETE FROM playlist WHERE PlaylistID = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $playlistID);
+
+    if ($stmt->execute()) {
+        header('Location: my_library.php'); // Redirect to the playlist library or any other page
+        exit();
+    } else {
+        // Handle deletion failure
+        echo "Error deleting playlist.";
+    }
+    } else {
+        // Handle deletion failure
+        echo "Error deleting playlist.";
+    }
+
+
+
+    $stmt->close();
+}
+
+// Add any additional code or error handling as needed
+?>
--- getPlaylist.php
+++ getPlaylist.php
@@ -6,25 +6,21 @@ include('connection.php');
 $playlistID = $_POST['playlistID'];
 $playlist = new Playlist($con, $playlistID);
 
-
 echo '<h1 class="playlist-title">' . $playlist->getName() . '</h1>';
 
 echo '<ul class="song-list">';
-echo '<li>'; 
+echo '<li>';
 echo '<span class="song-title">Title</span>';
 echo '<span class="song-artist">Artist</span>';
 echo '<span class="song-duration">Duration</span>';
 echo '</li>';
 
-
 echo '<ul>';
-if ($playlist->isEmpty()){
+if ($playlist->isEmpty()) {
     echo '<div class="items-center justify-center md:pl-64 md:pr-64 mx-auto md:mt-10"><p style="color: gray;">This playlist is empty.</p></div>';
-}
-else {
+} else {
     foreach ($playlist->getSongIds() as $sid) {
         $song = new Song($con, $sid);
-
         $formattedDuration = formatDuration($song->getDuration());
 
         echo '<a href="#" ondblclick="playSong(\'' . $song->getSongID() . '\')">';
@@ -32,14 +28,22 @@ else {
         echo '<span class="song-title">' . $song->getTitle() . '</span>';
         echo '<span class="song-artist">' . $song->getArtistName() . '</span>';
         echo '<span class="song-duration">' . $formattedDuration . '</span>';
-        
         echo '</li>';
         echo '</a>';
     }
+
+    // Add Delete Playlist button
+    echo "<li>";
+    echo "<form method='post' action='deletePlaylist.php'>";
+    echo "<input type='hidden' name='playlistID' value='$playlistID'>";
+    echo "<input type='submit' name='deletePlaylist' value='Delete Playlist'>";
+    echo "</form>";
+    echo "</li>";
 }
 echo '</ul>';
 
-function formatDuration($durationInSeconds) {
+function formatDuration($durationInSeconds)
+{
     $minutes = floor($durationInSeconds / 60);
     $seconds = $durationInSeconds % 60;
 
