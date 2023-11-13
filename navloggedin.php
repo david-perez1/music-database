9aaf3cdebd17ad989673b4add7e11106b615a6b6
annekagill <136111606+annekagill@users.noreply.github.com>
2023-11-13 14:22:13 -0600

    Update navloggedin.php

--- navloggedin.php
+++ navloggedin.php
@@ -36,7 +36,7 @@ function setNavbarOpen($value)
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="shortcut icon" type="image/x-icon" href="Music header.ico">
+<link rel="shortcut icon" type="image/x-icon" href="icon.ico">
 <div>
 
   <nav class="bg-gray-100">
