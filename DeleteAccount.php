20ae9fe9debac990e205af2942f6632713789bfc
david-perez1 <143773766+david-perez1@users.noreply.github.com>
2023-11-12 20:11:06 -0600

    Update DeleteAccount.php

--- DeleteAccount.php
+++ DeleteAccount.php
@@ -3,7 +3,7 @@ include('connection.php');
 include('navloggedin.php');
 
 if (!isset($_SESSION['id'])) {
-    header('Location: login.php'); // Redirect to login page if not logged in
+    header('Location: index.php'); // Redirect to login page if not logged in
     exit;
 }
 
