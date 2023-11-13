25a5db5d4ddb19fc03f595034c54977639016fac
arandrews <116930513+arandrews@users.noreply.github.com>
2023-11-13 01:22:09 -0600

    Update create_playlist.php

--- create_playlist.php
+++ create_playlist.php
@@ -6,6 +6,6 @@ session_start();
 $playlistName = $_POST['playlistName'];
 $todayDate = date("Y-m-d");
 $userID = $_SESSION['id'];
-$sql = "INSERT INTO playlist (`Playlist Title`, UserID, CreatedDate) VALUES ('$playlistName', '$userID', '$todayDate')";
+$sql = "INSERT INTO playlist (`PlaylistTitle`, UserID, CreatedDate) VALUES ('$playlistName', '$userID', '$todayDate')";
 $conn->query($sql);
 ?>
