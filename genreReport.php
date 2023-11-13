104b41ebedea35358e9695dddee88b340371e816
david-perez1 <143773766+david-perez1@users.noreply.github.com>
2023-11-13 16:12:39 -0600

    Update genreReport.php

--- genreReport.php
+++ genreReport.php
@@ -11,14 +11,14 @@ $yearColumn = '';
 // Check if the year filter is set
 if (isset($_GET['year']) && !empty($_GET['year'])) {
     $year = $conn->real_escape_string($_GET['year']);
-    $filter = "WHERE YEAR(ReleaseDate) = $year";
+    $filter .= "WHERE YEAR(ReleaseDate) = $year";
     $yearColumn = ", YEAR(ReleaseDate) AS Year";
 }
 
 // Check if the specific date filter is set
 if (isset($_GET['releaseDate']) && !empty($_GET['releaseDate'])) {
     $date = $conn->real_escape_string($_GET['releaseDate']);
-    $filter = "WHERE ReleaseDate = '$date'";
+    $filter .= "WHERE ReleaseDate = '$date'";
     $yearColumn = ", YEAR(ReleaseDate) AS Year";
 }
 
@@ -26,7 +26,7 @@ if (isset($_GET['releaseDate']) && !empty($_GET['releaseDate'])) {
 if (isset($_GET['startYear'], $_GET['endYear']) && !empty($_GET['startYear']) && !empty($_GET['endYear'])) {
     $startYear = $conn->real_escape_string($_GET['startYear']);
     $endYear = $conn->real_escape_string($_GET['endYear']);
-    $filter = "WHERE YEAR(ReleaseDate) BETWEEN $startYear AND $endYear";
+    $filter .= "WHERE YEAR(ReleaseDate) BETWEEN $startYear AND $endYear";
     $yearColumn = ", YEAR(ReleaseDate) AS Year";
 }
 
@@ -34,7 +34,7 @@ if (isset($_GET['startYear'], $_GET['endYear']) && !empty($_GET['startYear']) &&
 if (isset($_GET['startDate'], $_GET['endDate']) && !empty($_GET['startDate']) && !empty($_GET['endDate'])) {
     $startDate = $conn->real_escape_string($_GET['startDate']);
     $endDate = $conn->real_escape_string($_GET['endDate']);
-    $filter = "WHERE ReleaseDate BETWEEN '$startDate' AND '$endDate'";
+    $filter .= "WHERE ReleaseDate BETWEEN '$startDate' AND '$endDate'";
     $yearColumn = ", YEAR(ReleaseDate) AS Year";
 }
 // Check if the country filter is set
@@ -47,7 +47,7 @@ if (isset($_GET['country']) && !empty($_GET['country'])) {
 if (isset($_GET['combinedYear'], $_GET['combinedCountry']) && !empty($_GET['combinedYear']) && !empty($_GET['combinedCountry'])) {
     $combinedYear = $conn->real_escape_string($_GET['combinedYear']);
     $combinedCountry = $conn->real_escape_string($_GET['combinedCountry']);
-    $filter = "WHERE YEAR(ReleaseDate) = $combinedYear AND a.Country = '$combinedCountry'";
+    $filter .= "WHERE YEAR(ReleaseDate) = $combinedYear AND a.Country = '$combinedCountry'";
     $yearColumn = ", YEAR(ReleaseDate) AS Year";
 }
 
@@ -98,18 +98,21 @@ echo '<div class="filter-section">';
 //       </form>';
 
 
+ 
 
-echo '<label for="combinedCountry" class="filter-label">Filter by Year:</label>
-            <select id="combinedCountry" name="combinedCountry">
+// Dropdowns for year range
+echo '<form action="" method="get" class="filter-form">
+        <div class="year-range-filter">
+            <label for="year" class="filter-label">Filter by Year:</label>
+            <select id="year" name="year">
                 <option value="">Select Year</option>';
-foreach ($countries as $country) {
-    echo '<option value="'.htmlspecialchars($country).'">'.htmlspecialchars($country).'</option>';
+foreach ($years as $year) {
+    echo '<option value="'.htmlspecialchars($year).'">'.htmlspecialchars($year).'</option>';
 }
-echo '</select>
-            <input type="submit" value="Filter by Year" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" style="width: 200px">
-        </div>
-      </form>';
 
+echo '</select>
+        <input type="submit" value="Filter by Year" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" style="width: 200px">
+      </form>';
 
 
 
@@ -166,11 +169,17 @@ echo '<form action="" method="get" class="filter-form">
       </form>';
 
 // filter by country
-echo '<form action="" method="get" class="filter-form">
-        <label for="country" class="filter-label">Filter by Country:</label>
-        <input type="text" id="country" name="country">
-        <input type="submit" value="Filter by Country" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" style="width: 200px">
+echo '<label for="combinedCountry" class="filter-label">Filter by Country:</label>
+            <select id="combinedCountry" name="combinedCountry">
+                <option value="">Select Country</option>';
+foreach ($countries as $country) {
+    echo '<option value="'.htmlspecialchars($country).'">'.htmlspecialchars($country).'</option>';
+}
+echo '</select>
+            <input type="submit" value="Filter by Year" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" style="width: 200px">
+        </div>
       </form>';
+
 
 echo '<form action="" method="get" class="filter-form">
         <label for="combinedYear" class="filter-label">Year:</label>
@@ -206,5 +215,5 @@ echo '</table>';
 <link href="allWebApp.css" rel="stylesheet">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Top 10</title>
+<title>Top 10 Genre</title>
 </head>
