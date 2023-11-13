5e363f767dc2d8b05d8e88b5fe569fad1f559b16
arandrews <116930513+arandrews@users.noreply.github.com>
2023-11-13 01:20:24 -0600

    Update getPlaylists.php

--- getPlaylists.php
+++ getPlaylists.php
@@ -6,6 +6,6 @@ $sql = "SELECT * FROM playlist WHERE playlist.UserID = $uid";
 $playlists = mysqli_query($con, $sql);
 
 foreach ($playlists as $playlistData) {
-    echo "<a href='#' onclick='showPlaylist(\"" . $playlistData["PlaylistID"] . "\")'><li>" . $playlistData['Playlist Title'] . "</li></a>";
+    echo "<a href='#' onclick='showPlaylist(\"" . $playlistData["PlaylistID"] . "\")'><li>" . $playlistData['PlaylistTitle'] . "</li></a>";
 }
 ?>
