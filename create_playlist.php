0c763e76fb3e9d4db6cd84b2d932abb41838476a
Yonatan <yonatandeneke@gmail.com>
2023-11-26 00:18:00 -0600

    playlist amount limitation

--- create_playlist.php
+++ create_playlist.php
@@ -8,7 +8,23 @@ $playlistImage = $_POST['image'];
 $todayDate = date("Y-m-d");
 $userID = $_SESSION['id'];
 
-$sql = "INSERT INTO playlist (`PlaylistTitle`, `UserID`, `CreatedDate`, `image`) VALUES ('$playlistName', '$userID', '$todayDate', '$playlistImage')";
-$conn->query($sql);
+try {
+    $sql = "INSERT INTO playlist (`PlaylistTitle`, `UserID`, `CreatedDate`, `image`) VALUES ('$playlistName', '$userID', '$todayDate', '$playlistImage')";
+    $conn->query($sql);
 
+    if ($conn->errno) {
+        throw new Exception($conn->error);
+    }
+
+} catch (Exception $e) {
+    $errorMessage = $e->getMessage();
+
+    if (strpos($errorMessage, 'User already has 10 playlists') !== false) {
+        echo "<script>alert('You cannot have more than 10 playlists!');</script>";
+    } else {
+        error_log("Error: " . $errorMessage, 3, "error_log.txt");
+
+        echo "<script>alert('An error occurred. Please try again later.');</script>";
+    }
+}
 ?>
