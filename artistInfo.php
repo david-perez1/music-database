fa0b4b2e05bc39299db002d589a6d684c66792f8
annekagill <136111606+annekagill@users.noreply.github.com>
2023-11-25 23:58:03 -0600

    Update artistInfo.php

--- artistInfo.php
+++ artistInfo.php
@@ -1,31 +1,30 @@
 <?php
-include('connection.php'); // Include your database connection file
+include('connection.php'); 
 
-// Check if ArtistName is set in the URL
+
 if (isset($_GET['artistName'])) {
     $artistName = $_GET['artistName'];
 
-    // Fetch artist information from the database
     $sql = "SELECT ArtistName, Biography FROM artist WHERE ArtistName = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('s', $artistName);
     $stmt->execute();
     $stmt->bind_result($artistName, $biography);
 
-    // Fetch the result
+    
     if ($stmt->fetch()) {
-        // Output artist information
+        
         echo "<h1>{$artistName}</h1>";
         echo "<p>{$biography}</p>";
     } else {
-        // Handle case where artist is not found
+        
         echo "Artist not found.";
     }
 
-    // Close the statement
+   
     $stmt->close();
 } else {
-    // Handle case where ArtistName is not set in the URL
+    
     echo "Invalid request. Please provide an ArtistName.";
 }
 ?>
